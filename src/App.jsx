d0689a4940482a3aa948c7c38a597dd35238ab0f
unknown <ok037352085@gmail.com>
2025-09-30 17:30:19 +0800

    chagne BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Home from './pages/Home'
 import Footer from './components/Footer/Footer'
